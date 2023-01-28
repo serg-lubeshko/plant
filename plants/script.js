882674e779da36581a6fadbd5c45aad2d51b37b0
Sergei_L <lubeshkosergei@gmail.com>
2023-01-29 00:22:43 +0300

    feat:: add widget for price

--- plants/script.js
+++ plants/script.js
@@ -15,3 +15,44 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 })
+
+
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     const button = document.querySelector('.not-open');
+//     const parent = button.parentNode;
+//
+//     button.addEventListener('click', function() {
+//         const classes = parent.classList;
+//         classes.toggle('not-open');
+//         classes.toggle('open');
+// })
+// });
+
+const button = document.querySelector('.not-open');
+const parent = button.parentNode;
+
+// button.onclick = function () {
+//     // body...
+//     console.log(parent)
+//
+//     alert('Hi');
+// };
+
+// const button = document.querySelector('button');
+// const parent = button.parentNode;
+//
+button.addEventListener('click', function() {
+
+    const classes = parent.classList;
+    console.log(classes)
+    classes.toggle('not-open');
+    classes.toggle('open');
+});
+// При нажатии на dropdown кнопку появляется описание тарифов цен в соответствии с макетом.
+// Внутри реализована кнопка order, которая ведет на секцию contacts, при нажатии на нее Accordion
+// все еще остается открытым. +25
+//
+// Пользователь может самостоятельно закрыть содержимое нажав на кнопку dropup,
+// но не может одновременно открыть все тарифы услуг, при открытии нового тарифа предыдущее
+// автоматически закрывается. +25
